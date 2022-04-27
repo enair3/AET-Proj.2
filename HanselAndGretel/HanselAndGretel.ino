3ed144d8c4f378a81e8ed6368f6293030aa370f3
enair3 <91293037+enair3@users.noreply.github.com>
2022-04-27 17:31:28 -0500

    incorporated song functions into HanselAndGretel.ino; cleaned up code and tested with circuit (works)

--- HanselAndGretel/HanselAndGretel.ino
+++ HanselAndGretel/HanselAndGretel.ino
@@ -1,22 +1,24 @@
 /* AET319M&T Project 2: Hansel and Gretel
     Gretel pushes witch into oven > gets key > saves Hansel
+    need to reupload to reset everything
+    NOTE: servos only turn after the song is finished
     arduino song credits to https://github.com/robsoncouto/arduino-songs
 */
 
 #include <Servo.h>
 #include "pitches.h"
+#include "songThemes.h"
 
 //inst servos (2)
 Servo ovenServo;
 Servo cageServo;
 
 //CONSTANTS. designate pin numbers. switch = I, pin = O
-const int witchSwitch = 2;
-const int cageSwitch = 3;
+const int witchSwitch = 3;
+const int cageSwitch = 2;
 
 const int witchServoPin = 10;
-const int cageServoPin = 4;
-const int ledPin = 7;
+const int cageServoPin = 12;
 const int piezoPin = 8;
 
 //VARIABLES. keep track of states
@@ -28,7 +30,6 @@ void setup() {
   pinMode(witchSwitch, INPUT);
   pinMode(cageSwitch, INPUT);
 
-  pinMode(ledPin, OUTPUT);
   pinMode(piezoPin, OUTPUT);
 
   ovenServo.attach(witchServoPin);
@@ -38,7 +39,6 @@ void setup() {
   ovenServo.write(0);
   cageServo.write(90);
 
-  digitalWrite(ledPin, LOW);
   digitalWrite(piezoPin, LOW);
 
   //debug
@@ -54,19 +54,15 @@ void loop() {
 void ovenBehavior() {
   witchState = digitalRead(witchSwitch); //read if HIGH/LOW, assign
 
-  //witch in oven, witchWwitch is ON but cage not opened yet. stays burnt
-  if (witchState == HIGH && cageState == LOW) {
-    ovenServo.write(180); //spin witch 180, show burnt side with key
-    digitalWrite(ledPin, HIGH);
-    tone(8, 2000); //CHANGE TONE TO WITCH SOUNDS
-    //might need inOven var (inst 0, dead 1) based on playtesting
-    //Q if players will close oven again after getting key...
+  //init states, start with witch out, hansel trapped in cage
+  if (witchState == LOW && cageState == LOW) {
+    witchTheme();
+    
+  //witch in oven, witchSwitch is ON but cage not opened yet. stays burnt
+  } if (witchState == HIGH && cageState == LOW) {
+    ovenServo.write(180);//spin witch 180, show burnt side with key
+    hanselCrying();
   }
-  //without else component, have to reupload to arduino to reset ovenServo
-  //  } else {
-  //    ovenServo.write(0);
-  //    digitalWrite(ledPin, LOW); //turn off witch sounds
-  //  }
 }
 
 //key/cage/hansel interaction
@@ -74,36 +70,76 @@ void cageBehavior() {
   cageState = digitalRead(cageSwitch);
 
   //key in cage keyhole AND witch in oven to free hansel
-  if (cageState == HIGH && witchState == HIGH) {
-    //  if (cageState == HIGH) {
-    noTone(8); //turn off Hansel crying
+  if (cageState == HIGH) {
+    victoryTheme();
     cageServo.write(0); //open door
-
-  } else { //no key, witch is out, still trapped
-    tone(8, 200); //CHANGE TONE FOR CRYING
-    cageServo.write(90); //closed door
   }
 }
 
-//arduino song functions
-//need to switch buzzer from source to piezoPin varName
-//format their code into here
-
-int tempo = 130;
-
-//int buzzer = 8; PIEZOPIN
-
+//SONGTHEME FUNCTIONS
+//WITCH SONG
 void witchTheme() {
   //FIX into here and check in ovenBehavior()
-  //https://github.com/robsoncouto/arduino-songs/blob/master/doom/doom.ino
+
+  int notes = sizeof(witchMelody) / sizeof(witchMelody[0]) / 2;
+  int wholenote = (60000 * 4) / witchTempo;
+  int divider = 0, noteDuration = 0;
+
+  // iterate over the notes of the melody.
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+    divider = pgm_read_word_near(witchMelody + thisNote + 1);
+    if (divider > 0) {
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5;
+    }
+    tone(piezoPin, pgm_read_word_near(witchMelody + thisNote), noteDuration * 0.9);
+    delay(noteDuration);
+    noTone(piezoPin);
+  }
 }
 
+//HANSEL SONG
 void hanselCrying() {
   //FIX into here and check in cageBehavior()
-  //https://github.com/robsoncouto/arduino-songs/blob/master/bloodytears/bloodytears.ino
+  int notes = sizeof(hanselMelody) / sizeof(hanselMelody[0]) / 2;
+  int wholenote = (60000 * 4) / hanselTempo;
+  int divider = 0, noteDuration = 0;
+
+  // iterate over the notes of the melody.
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+    divider = pgm_read_word_near(hanselMelody + thisNote + 1);
+    if (divider > 0) {
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5;
+    }
+    tone(piezoPin, pgm_read_word_near(hanselMelody + thisNote ), noteDuration * 0.9);
+    delay(noteDuration);
+    noTone(piezoPin);
+  }
 }
 
+//VICTORY SONG
 void victoryTheme() {
   //FIX into here and check in cageBehavior()
-  //https://github.com/robsoncouto/arduino-songs/blob/master/nokia/nokia.ino
+  int notes = sizeof(victoryMelody) / sizeof(victoryMelody[0]) / 2;
+  int wholenote = (60000 * 4) / victoryTempo;
+  int divider = 0, noteDuration = 0;
+
+  // iterate over the notes of the melody.
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+    divider = victoryMelody[thisNote + 1];
+    if (divider > 0) {
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5;
+    }
+    tone(piezoPin, victoryMelody[thisNote], noteDuration * 0.9);
+    delay(noteDuration);
+    noTone(piezoPin);
+  }
 }
---
+++ HanselAndGretel/songThemes.h
@@ -0,0 +1,62 @@
+//song globals
+
+//WITCHTHEME GLOBALS
+int witchTempo = 225;
+const int witchMelody[] PROGMEM = {
+  // At Doom's Gate (E1M1)
+  // Score available at https://musescore.com/pieridot/doom
+
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8, //1
+  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_B2, 8, NOTE_C3, 8,
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8,
+  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, -2,
+
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8, //13
+  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_B2, 8, NOTE_C3, 8,
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8,
+  NOTE_FS3, -16, NOTE_D3, -16, NOTE_B2, -16, NOTE_A3, -16, NOTE_FS3, -16, NOTE_B2, -16, NOTE_D3, -16, 
+  NOTE_FS3, -16, NOTE_A3, -16, NOTE_FS3, -16, NOTE_D3, -16, NOTE_B2, -16
+};
+
+//HANSELCRYING GLOBALS
+int hanselTempo = 144;
+const PROGMEM int hanselMelody[] = {
+  // Bloody Tears, from Castlevania II
+  // Arranged by Bobby Lee. THe flute part was used
+  // https://musescore.com/user/263171/scores/883296
+
+  //B-flat major Bb Eb
+  REST, 4, NOTE_G5, 4,
+  NOTE_A5, 4, NOTE_AS5, 4,
+  NOTE_A5, 4, NOTE_F5, 4,
+  NOTE_A5, 4, NOTE_G5, 4,
+  REST, 4, NOTE_G5, 4,
+  NOTE_A5, 4, NOTE_AS5, 4,
+  NOTE_C6, 4, NOTE_AS5, 4,
+
+  NOTE_A5, 4, NOTE_G5, 4, //8
+  REST, 4, NOTE_G5, 4,
+  NOTE_A5, 4, NOTE_AS5, 4,
+  NOTE_A5, 4, NOTE_F5, 4,
+  NOTE_A5, 4, NOTE_G5, 4,
+  NOTE_D6, 4, REST, 8, NOTE_C6, 8,
+  REST, 4, NOTE_AS5, 4,
+
+  NOTE_A5, 4, NOTE_AS5, 8, NOTE_C6, 8, //15
+  NOTE_F6, 8, REST, 8, REST, 4,
+  NOTE_G5, 16, NOTE_D5, 16, NOTE_D6, 16, NOTE_D5, 16, NOTE_C6, 16, NOTE_D5, 16, NOTE_AS5, 16, NOTE_D5, 16,
+  NOTE_A5, 16, NOTE_D5, 16, NOTE_AS5, 16, NOTE_D5, 16, NOTE_A5, 16, NOTE_D5, 16, NOTE_G5, 16, NOTE_D5, 16,
+  NOTE_A5, 16, NOTE_D5, 16, NOTE_AS5, 16, NOTE_D5, 16, NOTE_C6, 16, NOTE_D5, 16, NOTE_AS5, 16, NOTE_D5, 16
+};
+
+//VICTORYTHEME GLOBALS
+int victoryTempo = 180;
+int victoryMelody[] = {
+  // Nokia Ringtone
+  // Score available at https://musescore.com/user/29944637/scores/5266155
+
+  NOTE_E5, 8, NOTE_D5, 8, NOTE_FS4, 4, NOTE_GS4, 4,
+  NOTE_CS5, 8, NOTE_B4, 8, NOTE_D4, 4, NOTE_E4, 4,
+  NOTE_B4, 8, NOTE_A4, 8, NOTE_CS4, 4, NOTE_E4, 4,
+  NOTE_A4, 2,
+};
